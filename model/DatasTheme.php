c732878becac5c1b0c684273f66e04c3990f63f5
Mikhawa <michaeljpitz@gmail.com>
2022-04-15 11:23:33 +0200

    theme 1

---
+++ model/DatasTheme.php
@@ -0,0 +1,33 @@
+<?php
+// contiendra les données pour l'affichage de l'exemple 'theme'
+class DatasTheme
+{
+    public const TITLE = [
+        'accueil'=>'Accueil de notre site',
+        'contact'=>'Contactez-moi',
+        'articles'=>[
+            'Installez Composer',
+            'Installez npm',
+            'installez Yarn',
+            ]
+        ];
+    public const SUBTITLE = [
+        'accueil'=>'Vous trouverez sur ce site comment installer des gestionnaires de package',
+        'contact'=>'Tu as un truc à dire? Vas-y',
+        'articles'=>[
+            'Installation de Composer',
+            'Installation de npm',
+            'Installation de Yarn',
+        ]
+    ];
+    public const SUBTEXT = [
+        'accueil'=>'Les packages seront <ul><li>Composer</li><li>Npm</li><li>Yarn</li></ul>',
+        'contact'=>'<input type="submit" type="submit" value="zyva!">',
+        'articles'=>[
+            'blablba Installation de Composer',
+            'blablba Installation de npm',
+            'blablba Installation de Yarn',
+        ]
+    ];
+
+}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,18 @@
 <?php
 // chargement de l'autoload de composer pour le chargement des dépendances du vendor
 require_once '../vendor/autoload.php';
+// chargement d'autres dépendance
+require_once '../model/DatasTheme.php';
 
 
 // on donne le chemin d'un dossier où se trouveront nos vues (ici des fichiers en .twig)
 $loader = new \Twig\Loader\FilesystemLoader('../view/theme');
 $twig = new \Twig\Environment($loader, []);
+
+// routeur dans notre CF
+if(isset($_GET['page'])){
+
+    // on est sur l'accueil
+}else{
+
+}
